b3915ebe895d7eb1f0987f8c86f502022000f68e
JGonz13x <154345982+JGonz13x@users.noreply.github.com>
2024-10-26 21:07:41 +0800

    add check pass to submit

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ const passwordError = document.querySelector(".forms-division");
 const submit = document.querySelector(".submit");
 
 submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  validatePassword(confirm);
+  validatePassword(e);
 });
 
-function validatePassword(confirm) {
+function validatePassword(e) {
   if (password.value !== confirmPassword.value) {
+    e.preventDefault();
     passwordError.classList.remove("password-match");
     passwordError.classList.add("password-not-match");
-  } else {
+  } else if (password.value && confirmPassword.value) {
     passwordError.classList.remove("password-not-match");
     passwordError.classList.add("password-match");
+  } else {
+    e.preventDefault();
+    passwordError.classList.remove("password-not-match");
+    passwordError.classList.remove("password-match");
   }
 }
