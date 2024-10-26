27f4b85594a319cf522a227b026fe7ff5ded473c
JGonz13x <154345982+JGonz13x@users.noreply.github.com>
2024-10-26 21:48:29 +0800

    added minimum password length

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 const passwordError = document.querySelector(".forms-division");
-
 const submit = document.querySelector(".submit");
 
+let passwordValue;
+let confirmPasswordValue;
+let isPasswordMatch = false;
+
 submit.addEventListener("click", (e) => {
-  validatePassword(e);
+  passwordValue = password.value;
+  confirmPasswordValue = confirmPassword.value;
+  validatePassword();
+  preventUnMatchPassword(e);
+  console.log(passwordValue);
 });
 
-function validatePassword(e) {
-  if (password.value !== confirmPassword.value) {
-    e.preventDefault();
+function validatePassword() {
+  if (passwordValue !== confirmPasswordValue) {
     passwordError.classList.remove("password-match");
     passwordError.classList.add("password-not-match");
-  } else if (password.value && confirmPassword.value) {
+    isPasswordMatch = false;
+  } else if (
+    passwordValue &&
+    confirmPasswordValue &&
+    passwordValue.length === 8 &&
+    confirmPasswordValue.length === 8
+  ) {
     passwordError.classList.remove("password-not-match");
     passwordError.classList.add("password-match");
+    isPasswordMatch = true;
   } else {
-    e.preventDefault();
     passwordError.classList.remove("password-not-match");
     passwordError.classList.remove("password-match");
+    isPasswordMatch = false;
   }
+}
+
+function preventUnMatchPassword(e) {
+  if (!isPasswordMatch) {
+    e.preventDefault();
+  }
+}
+
+function checkMatchingPassword() {
+  passwordValue = password.value;
+  confirmPasswordValue = confirmPassword.value;
+  validatePassword();
 }
